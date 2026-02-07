4335ce40f402fd79a141a64445545b06af350c21
TGJ Gilmore <tracyg.gilmore@gmail.com>
2026-02-07 21:13:56 +0000

    Canvas size dynamics

--- game-loop.js
+++ game-loop.js
@@ -1,8 +1,9 @@
 export default (motionInc, BOXES) => {
   const canvas = document.querySelector('canvas');
   const ctx = canvas.getContext('2d');
-  const MAX_FPS = 60;
-  const BOX_SIZE = 15;
+  const CANVAS_STEPS = 60;
+  const BOX_WIDTH = canvas.width / CANVAS_STEPS;
+  const BOX_HEIGHT = canvas.height / BOXES.length;
   const LOOP = false;
   const boxes = BOXES.map(({ speed, pos }) => ({
     speed,
@@ -14,8 +15,7 @@ export default (motionInc, BOXES) => {
   function updateState() {
     let stateChanged = [];
     boxes.forEach(({ speed, pos }, idx) => {
-      const stepSize = MAX_FPS / speed;
-      const step = stepCount % stepSize;
+      const step = stepCount % speed;
 
       if (!step) {
         stateChanged.push(speed);
@@ -28,15 +28,15 @@ export default (motionInc, BOXES) => {
   function draw(timestamp) {
     const updateCount = updateState();
     if (updateCount) {
-      ctx.clearRect(0, 0, 900, 180);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
       boxes.forEach(({ pos }, idx) => {
         ctx.fillStyle = `oklch(50% 100% ${idx * 30}deg)`;
-        ctx.fillRect(pos * BOX_SIZE, idx * BOX_SIZE, BOX_SIZE, BOX_SIZE); // x,y,w,h
+        ctx.fillRect(pos * BOX_WIDTH, idx * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT); // x,y,w,h
       });
     }
     const dateNow = Date();
     $timestamp.textContent = timestamp;
-    $stepCount.textContent = `${stepCount} (${stepCount % MAX_FPS})`;
+    $stepCount.textContent = `${stepCount} (${stepCount % CANVAS_STEPS})`;
     $dateNow.textContent = dateNow;
     $updateCount.textContent = updateCount;
     console.log(
@@ -46,7 +46,7 @@ export default (motionInc, BOXES) => {
       `${updateCount}`.padStart(30),
     );
     stepCount++;
-    (LOOP || stepCount < (MAX_FPS - 1 + motionInc) / motionInc) &&
+    (LOOP || stepCount < (CANVAS_STEPS - 1 + motionInc) / motionInc) &&
       requestAnimationFrame(draw);
   }
 
